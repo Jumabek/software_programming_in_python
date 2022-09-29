24a72ba5ab56969518d38aeba9d2eafadfbd7598
jumabek <jumabek4044@gmail.com>
2022-09-29 18:58:28 +0900

    end of class

--- week5_allMyCats2.py
+++ week5_allMyCats2.py
@@ -1,11 +1,12 @@
-catNames = []
-while True:
+catNames = [] # [] creates empty list
+while True: # this runs all the time
     print('Enter the name of cat ' + str(len(catNames) + 1) +
       ' (Or enter nothing to stop.):')
     name = input()
     if name == '':
         break
-    catNames = catNames + [name]  # list concatenation
+    #catNames = catNames + [name]  # list concatenation
+    catNames +=  [name]
 print('The cat names are:')
 for name in catNames:
     print('  ' + name)
